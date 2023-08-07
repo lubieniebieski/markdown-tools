826288f2f4c4051b73789f2c52a9ca82068f352d
Adam Nowak <adam.nowak@netguru.com>
2023-08-07 17:47:37 +0200

    update version

--- cmd/root.go
+++ cmd/root.go
@@ -27,7 +27,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var version = "0.1-alpha"
+var version = "0.2"
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
